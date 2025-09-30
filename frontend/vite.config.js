dd24cd480101bd2b6be30578f8cfed9ff44a3466
Ragav8373 <Ragav9760@gmail.com>
2025-09-30 16:43:37 +0530

    updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/",   // ✅ ensures correct asset paths on Netlify
   server: {
-    host: true,      // <-- listen on all network interfaces
-    port: 5173,      // optional, set your desired port
+    host: true,  // listen on all network interfaces
+    port: 5173,  // optional (dev only)
   },
 })
